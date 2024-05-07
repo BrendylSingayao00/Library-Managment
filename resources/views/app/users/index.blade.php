c5bf8dd20ed8599a9550eb3fc52218a64650629f
BrendylSingayao00 <2101101088@student.buksu.edu.ph>
2024-05-07 10:40:53 +0800

    User index design

--- resources/views/app/users/index.blade.php
+++ resources/views/app/users/index.blade.php
@@ -1,49 +1,68 @@
 <x-user-layout>
+    @role('admin')
+    <div class="top">
+        <i class="uil uil-bars sidebar-toggle"></i>
+        <div class="search-box">
+            <i class="uil uil-search"></i>
+            <input type="text" placeholder="Search here...">
+        </div>
+    </div>
+    <div class="dash-content">
+        <div class="overview">
+            <div class="title">
+                <i class="uil uil-tachometer-fast-alt"></i>
+                <span class="text">Borrower</span>
+            </div>
+            <div>
+                <x-btn-link class="ml-4 float-right" href="{{ route('users.create')}}">
+                    New Borrower</x-btn-link>
+            </div>
 
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('Borrower') }}
-        <x-btn-link class="ml-4 float-right" href="{{ route('users.create')}}">
-            New Borrower</x-btn-link>
-    </h2>
-
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-
-                    <div class="relative over-flow-x-auto">
-                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                            <thead
-                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <div class="activity">
+                <div class="activity-data">
+                    <div class="data">
+                        <table class="user_table">
+                            <thead>
                                 <tr>
-                                    <th scope="col" class="px-6 py-3">
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <span class="text-gray-500">
+                                            ID
+                                        </span>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
                                         Name
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Email
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Role
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Action
-                                    </th>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <span class="text-gray-500">
+                                            Email
+                                        </span>
+                                    </td>
+
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <span class="text-gray-500">
+                                            Role
+
+                                        </span>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <span class="text-gray-900">
+                                            Action
+                                        </span>
+                                    </td>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach($users as $user)
+                                @if (!$loop->first)
                                 <tr>
                                     <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="flex items-center">
-
-                                            <div class="ml-4">
-                                                <!-- Add your product name here -->
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    {{$user->name}}
-                                                </div>
-                                            </div>
-                                        </div>
+                                        <!-- Add your product color here -->
+                                        <span class="text-gray-500">
+                                            2100102
+                                        </span>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{$user->name}}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <!-- Add your product color here -->
@@ -64,23 +83,20 @@
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <!-- Add your product price here -->
                                         <span class="text-gray-900">
-                                            <x-btn-link href="{{ route('users.edit',$user->id)}}">Edit</x-btn-link>
+                                            <x-btn-link href="{{ route('users.edit',$user->id)}}">Edit
+                                            </x-btn-link>
                                         </span>
                                     </td>
                                 </tr>
-                                <!-- Add more rows with your data as needed -->
+                                @endif
                                 @endforeach
                             </tbody>
                         </table>
+
                     </div>
-
-
                 </div>
             </div>
         </div>
     </div>
+    @endrole
 </x-user-layout>
-
-
-
-<!-- 47:03 -->
--- resources/views/components/user-layout.blade.php
+++ resources/views/components/user-layout.blade.php
@@ -36,219 +36,127 @@
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/2.0.6/css/dataTables.dataTables.css" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sidebar.css" rel="stylesheet">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="css/noanimation.css" rel="stylesheet">
-
+    <link href="css/user.css" rel="stylesheet">
+    <script src="https://cdn.datatables.net/2.0.6/js/dataTables.bootstrap5.js"></script>
+    <script src="https://cdn.datatables.net/2.0.6/js/dataTables.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js
+"></script>
 
 
 </head>
 
-<body id="page-top">
-
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-            </a>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
+<body>
+    <nav>
+        <div class="logo-name">
+            <div class="logo-image">
+                <img src="images/logo.png" alt="">
+            </div>
+            <span class="logo_name">CodingLab</span>
+        </div>
+        <div class="menu-items">
             @role('admin')
-            <li class="nav-item">
-                <a class="nav-link" href="/dashboard">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
+            <ul class="nav-links">
+                <li><a href="{{ route('dashboard')}}">
+                        <i class="uil uil-estate"></i>
+                        <span class="link-name">Dahsboard</span>
+                    </a></li>
+                <li><a href="{{ route('users.index')}}">
+                        <i class="uil uil-files-landscapes"></i>
+                        <span class="link-name">Borrower</span>
+                    </a></li>
+
+                <li><a href="">
+                        <i class="uil uil-files-landscapes"></i>
+                        <span class="link-name">Borrowing</span>
+                    </a></li>
+
+                <li><a href="">
+                        <i class="uil uil-files-landscapes"></i>
+                        <span class="link-name">Add Books</span>
+                    </a></li>
+                @endrole
+                <li><a href="">
+                        <i class="uil uil-files-landscapes"></i>
+                        <span class="link-name">Books</span>
+                    </a></li>
+
+                <li><a href="">
+                        <i class="uil uil-files-landscapes"></i>
+                        <span class="link-name">Borrower</span>
+                    </a></li>
 
 
 
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('users.index')}}">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Borrower</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Borrowing</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('app.student')}}">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Add Books</span></a>
-            </li>
-            @endrole
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('app.student')}}">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Books</span></a>
-            </li>
+            </ul>
 
-
-
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Announcement</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Activities</span></a>
-            </li>
-
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-
-
-        </ul>
-        <!-- End of Sidebar -->
-
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
-            <div id="content">
-
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
-                    <!-- Topbar Search -->
-                    <form
-                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                                aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
-
-
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
-
-
-                        <!-- Settings Dropdown -->
-                        <div class="hidden sm:flex sm:items-center sm:ms-6">
-                            <x-dropdown align="right" width="48">
-                                <x-slot name="trigger">
-                                    <button
-                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                        <div>{{ Auth::user()->name }}</div>
-
-                                        <div class="ms-1">
-                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd"
-                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </div>
-                                    </button>
-                                </x-slot>
-
-                                <x-slot name="content">
-                                    <x-dropdown-link :href="route('profile.edit')">
-                                        {{ __('Profile') }}
-                                    </x-dropdown-link>
-
-                                    <!-- Authentication -->
-                                    <form method="POST" action="{{ route('logout') }}">
-                                        @csrf
-
-                                        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                            {{ __('Log Out') }}
-                                        </x-dropdown-link>
-                                    </form>
-                                </x-slot>
-                            </x-dropdown>
-                        </div>
-
-
-
-
-                    </ul>
-
-                </nav>
-                <!-- End of Topbar -->
-
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
-
-                    {{ $slot }}
-                </div>
-            </div>
-
-
+            <ul class="logout-mode">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <li><a href="route('logout')" onclick="event.preventDefault();
+                                          this.closest('form').submit();">
+                            <i class="uil uil-signout"></i>
+                            <span class="link-name">Logout</span>
+                        </a>
+                    </li>
+                </form>
+                <li class="mode">
+                    <a href="#">
+                        <i class="uil uil-moon"></i>
+                        <span class="link-name">Dark Mode</span>
+                    </a>
+                    <div class="mode-toggle">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+            </ul>
         </div>
+    </nav>
 
-    </div>
-    <!-- End of Content Wrapper -->
-
-    </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <main class="dashboard">
+        {{ $slot }}
+    </main>
 
 </body>
+<script>
+const body = document.querySelector("body"),
+    modeToggle = body.querySelector(".mode-toggle");
+sidebar = body.querySelector("nav");
+sidebarToggle = body.querySelector(".sidebar-toggle");
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+    body.classList.toggle("dark");
+}
+
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+    sidebar.classList.toggle("close");
+}
+
+modeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
+        localStorage.setItem("mode", "dark");
+    } else {
+        localStorage.setItem("mode", "light");
+    }
+});
+
+sidebarToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    if (sidebar.classList.contains("close")) {
+        localStorage.setItem("status", "close");
+    } else {
+        localStorage.setItem("status", "open");
+    }
+})
+</script>
 
 </html>
